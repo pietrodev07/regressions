bb12c01c5965fda4310a09ccb2bf956671a75d5c
pietrodev07 <pietro.dev.07@gmail.com>
2025-05-11 11:36:08 +0200

    feat(regressions): add linear regression example

---
+++ src/datasets/index.ts
@@ -0,0 +1,11 @@
+import type { TrainData } from "#/types";
+
+export const housePricesData: TrainData = [
+  [900, 200],
+  [1200, 250],
+  [1500, 300],
+  [1800, 350],
+  [2000, 400],
+  [2200, 450],
+  [2500, 500],
+];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("regressions");
+import { housePricesData } from "#/datasets";
+import { LinearRegression } from "#/algorihtms";
+
+console.log("\n============= HOUSING PRICES (Linear Regression) =============\n");
+
+const housingModel = new LinearRegression();
+housingModel.train(housePricesData);
+
+const housingEquation = housingModel.getEquation();
+const predictedPrice = housingModel.predict(1600);
+
+console.table(housePricesData.map(([x, y]) => ({ SquareFeet: x, Price: y })));
+console.log(`Equation: y = ${housingEquation.slope}x + ${housingEquation.intercept}`);
+console.log(`Predicted price for 1600 sqft: $${predictedPrice}K`);
